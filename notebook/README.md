bdd07a81c62fbdb4f4b0889020142cea9aa8c90b
Matthew J. Collins <mcollins@acis.ufl.edu>
2016-03-17 17:05:54 -0400

    Some running of more realistic queries

---
+++ notebook/README.md
@@ -0,0 +1,1 @@
+The 00 file goes in ~/.ipython/profile_pyspark/startup/00-pyspark-setup.py
--- notebook/nb.sh
+++ notebook/nb.sh
@@ -3,4 +3,5 @@
 # http://ramhiser.com/2015/02/01/configuring-ipython-notebook-support-for-pyspark/
 export SPARK_HOME="/opt/spark"
 #export PYSPARK_SUBMIT_ARGS="--master cloudera0.acis.ufl.edu"
+#export PYSPARK_SUBMIT_ARGS="--executor-memory 3G --num-executors 3 --executor-cores 5 pyspark-shell "
 ipython notebook --profile=pyspark --ip='*' --no-browser
