9e11789be194201e862f76b8bd0a8f9879de007f
Matthew J. Collins <mcollins@acis.ufl.edu>
2016-03-27 14:04:44 -0400

    Changes of varying kinds, quick unique values notebook

--- notebook/nb.sh
+++ notebook/nb.sh
@@ -3,5 +3,5 @@
 # http://ramhiser.com/2015/02/01/configuring-ipython-notebook-support-for-pyspark/
 export SPARK_HOME="/opt/spark"
 #export PYSPARK_SUBMIT_ARGS="--master cloudera0.acis.ufl.edu"
-#export PYSPARK_SUBMIT_ARGS="--executor-memory 3G --num-executors 3 --executor-cores 5 pyspark-shell "
+export PYSPARK_SUBMIT_ARGS="--executor-memory 3G --num-executors 3 --executor-cores 5 --jars ${SPARK_HOME}/jars/elasticsearch-hadoop-2.2.0.jar pyspark-shell "
 ipython notebook --profile=pyspark --ip='*' --no-browser
