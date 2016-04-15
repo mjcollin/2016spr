7fd3e82c449ebf46e369d2a8c2bf534cb6b17607
Matthew J. Collins <mcollins@acis.ufl.edu>
2016-04-15 14:38:38 -0400

    Change return to allow for original tuples to come out since we'll need them for chunking

--- notebook/lib/pos_tags.py
+++ notebook/lib/pos_tags.py
@@ -2,12 +2,23 @@ import nltk
 
 class PosTags:
 
-    def tag(self, t):
+    def tag(self, t, as_dicts=True):
         '''
         With a list of tokens, mark their part of speech and return
         a list dicts (no native tuple type in dataframes it seems).
         '''
         pos = nltk.pos_tag(t)
+        if as_dicts:
+            return self.to_dicts(pos)
+        else:
+            return pos
+
+
+    def to_dicts(self, pos):
+        '''
+        With a list of POS tag tuples, convert the tuples to dicts
+        because Spark can't store tuples.
+        '''
         retval = []
         for p in pos:
             retval.append({"word": p[0], "tag": p[1]})
