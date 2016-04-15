303e45e7f273bb719bc6d9d2ccde4249ea074388
Matthew J. Collins <mcollins@acis.ufl.edu>
2016-04-15 10:16:37 -0400

    Initial chinking work

---
+++ notebook/lib/chunks.py
@@ -0,0 +1,10 @@
+class Chunks:
+
+    def chunk(self, l):
+        pass
+
+
+if __name__ == "__main__":
+
+    chunks = Chunks()
+    print(chunks.chunk(["The", "tall", "oak", "in", "the", "green", "forest", ":", "45"]))
