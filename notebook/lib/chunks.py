d4cff894b37f75a37bbfb25758624a4f77f39b1a
Matthew J. Collins <mcollins@acis.ufl.edu>
2016-04-18 13:49:38 -0400

    Inital NB chunking classifier

--- notebook/lib/chunks.py
+++ notebook/lib/chunks.py
@@ -1,3 +1,6 @@
+import nltk
+import json
+
 class Chunks:
 
     # Right now we are doing no chunking of tokens to try
@@ -6,17 +9,46 @@ class Chunks:
         return l
 
 
-    # FIXME: this function is a cut-paste, need to pick a
-    # format for storing chunks
-    def to_dicts(self, pos):
+    def extract_features(self, sentence, i):
+        return {"tag": sentence[i]["tag"]}
+
+
+    def load_training_data(self, fn):
+        with open(fn) as f:
+            self.training_data = []
+            for l in f:
+                self.training_data.append(json.loads(l))
+
+
+    def train(self, fn):
+        self.load_training_data(fn)
+        print(self.training_data)
+
+        train_set = []
+        for sentence in self.training_data:
+            for i, pos_tagged in enumerate(sentence):
+                print(pos_tagged)
+                train_set.append( (self.extract_features(sentence, i), pos_tagged["iob"]) )
+
+        print(train_set)
+        print(type(train_set))
+        self.classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+
+    def predict(self, sentence):
+        iob_tagged = []
+        for i, pos_tagged in enumerate(sentence):
+            iob = self.classifier.classify(self.extract_features(sentence, 1))
+            iob_tagged.append( {"word": pos_tagged["word"], "tag": pos_tagged["tag"], "iob": iob} )
+        return iob_tagged
+
+
+    def assemble(self, iob_tagged):
         '''
-        With a list of POS tag tuples, convert the tuples to dicts
-        because Spark can't store tuples.
+        Re-format a list of iob tagged words into a pos tagged
+        style where the words are concatenated together.
         '''
-        retval = []
-        for p in pos:
-            retval.append({"word": p[0], "tag": p[1]})
-        return retval
+        return False
 
 
 if __name__ == "__main__":
@@ -29,12 +61,17 @@ if __name__ == "__main__":
     pos_tags = PosTags()
     chunker = Chunks()
 
+    chunker.train("tests/chunk_training.json")
+
     with open(fn) as f:
         for l in f:
             tokens = t.tokenize(l)
-            pos = pos_tags.tag(tokens, as_dicts=False)
-            chunks = chunker.chunk(pos)
-            print(chunks)
+            pos = pos_tags.tag(tokens)
+            print(chunker.predict(pos))
+            #features = chunker.extract_features(pos, 0)
+            #print(features)
+            #chunks = chunker.chunk(pos)
+            #print(chunks)
             #s = ""
             #for p in pos:
             #    s = s + p["word"] + " (" + p["tag"] + ") | "
